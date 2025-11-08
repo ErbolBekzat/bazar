d5145c86b73822dcad914d1592d8e62581e5e3dc
Erbol <dj.erbol.b@gmail.com>
2025-11-08 23:04:05 +0600

    layout, await was removed

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -14,7 +14,7 @@ export default async function RootLayout({
   children: ReactNode;
   params: { lang: "en" | "ru" | "ky" };
 }) {
-  const { lang } = await params;
+  const { lang } = params;
   const dict = await getDictionary(lang);
 
   return (
