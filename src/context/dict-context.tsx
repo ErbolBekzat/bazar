5d15b2028b4d8db45045b1382c7844ec627187d1
Erbol <dj.erbol.b@gmail.com>
2025-11-08 23:43:59 +0600

    dict context updated

--- src/context/dict-context.tsx
+++ src/context/dict-context.tsx
@@ -1,7 +1,42 @@
 "use client";
 import { createContext, useContext } from "react";
 
-export type Dict = any;
+export type Dict = {
+  nav: {
+    home: string;
+    shops: string;
+    profile: string;
+    login: string;
+  };
+  homePage: {
+    discover: string;
+    description: string;
+    exploreStores: string;
+    addMyStore: string;
+    featuredShops: string;
+  };
+  product: {
+    add: string;
+    price: string;
+    stock: string;
+  };
+  shopDirectory: {
+    shopsDirectory: string;
+  };
+  shopPage: {
+    description: string;
+    shopNotFound: string;
+    products: string;
+  };
+  footer: {
+    copyright: string;
+    contact: string;
+  };
+  button: {
+    visitShop: string;
+  };
+};
+
 export const DictContext = createContext<Dict | null>(null);
 
 export const useDict = () => {
